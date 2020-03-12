e4353272f42219508a32081912ccd3fd90f0241c
Bartholomew Fountain <baertholomew@gmail.com>
2020-03-12 00:29:59 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,6 @@
 def rescue_princess_peach
-  status = "rescued"rescue_princess_peach
-puts status
-  puts "Hooray! Mario has rescued Princess Peach."
+  status = "rescued"
+  prescue_princess_peach
+puts statusuts "Hooray! Mario has rescued Princess Peach."
 end
 
